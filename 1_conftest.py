9a135fc893ac79635d83cb21a525a54712d4f0bb
ChithurajMsys <95626625+ChithurajMsys@users.noreply.github.com>
2021-12-28 13:18:02 +0530

    Update 1_conftest.py

--- 1_conftest.py
+++ 1_conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from base.webdriverfactory import WebDriverFactory
-from pages.home.login_page import LoginPage
+from pages.home..login_page import LoginPage
 
 @pytest.yield_fixture()
 def setUp():
