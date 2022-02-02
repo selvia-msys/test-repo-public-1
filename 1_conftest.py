7c35a6783d06e13e7cd30f285dacb3e14ff3ac99
ChithurajMsys <95626625+ChithurajMsys@users.noreply.github.com>
2022-02-02 16:22:49 +0530

    Update 1_conftest.py

--- 1_conftest.py
+++ 1_conftest.py
@@ -2,6 +2,8 @@ import pytest
 from base.webdriverfactory import WebDriverFactory
 from pages.home......login_page import LoginPage
 
+
+
 @pytest.yield_fixture()
 def setUp():
     print("Running method level setUp")
